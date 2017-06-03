beba3f2f4eb4c265c342d8e4f461166cc7c22988
shigeru10 <shiiiiigeeeee10@gmail.com>
2017-06-03 22:27:08 +0900

    create table view

--- tableViewSample/ViewController.swift
+++ tableViewSample/ViewController.swift
@@ -8,18 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    private let myiPhoneItems: NSArray = ["iOS9", "iOS8", "iOS7", "iOS6", "iOS5", "iOS4"]
+    private let myAndroidItems: NSArray = ["5.x", "4.x", "3.x", "2.x"]
+    
+    private let mySections: NSArray = ["iPhone", "Android"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        let myTableView: UITableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        myTableView.dataSource = self
+        
+        myTableView.delegate = self
+        
+        self.view.addSubview(myTableView)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return mySections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return mySections[section] as? String
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            print("Value: \(myiPhoneItems[indexPath.row])")
+        } else if indexPath.section == 1 {
+            print("Value: \(myAndroidItems[indexPath.row])")
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return myiPhoneItems.count
+        } else if section == 1 {
+            return myAndroidItems.count
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = String(describing: myiPhoneItems[indexPath.row])
+        } else if indexPath.section == 1 {
+            cell.textLabel?.text = String(describing: myAndroidItems[indexPath.row])
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let myShareButton: UITableViewRowAction = UITableViewRowAction(style: .normal, title: "Share") { (action, index) -> Void in
+            tableView.isEditing = false
+            print("Share")
+        }
+        
+        myShareButton.backgroundColor = UIColor.blue
+        
+        let myArchiveButton: UITableViewRowAction = UITableViewRowAction(style: .normal, title: "Archive") { (action, index) -> Void in
+            tableView.isEditing = false
+            print("Archive")
+        }
+        
+        myArchiveButton.backgroundColor = UIColor.red
+        
+        return [myShareButton, myArchiveButton]
+    }
 }
 
